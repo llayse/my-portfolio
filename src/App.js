fb4a377e0fee46777b85d82ad89e381f86f67cbb
Layse C S Garcia <m309612@alunos.ipb.pt>
2024-06-01 20:20:17 +0100

    Adding new styles and updating App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 import { Skills } from './components/Skills';
-
+import { Projects } from './components/Projects';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <NavBar/>
       <Banner></Banner>
       <Skills></Skills>
+      <Projects></Projects>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
